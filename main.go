d622fbd15185ff60eee4828bc69d0db0f235398a
DavidLSaldana <dleosaldana@gmail.com>
2025-05-15 08:21:27 -0700

    adding readiness endpoint

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	filepathRoot := "/"
+	filepathApp := "/app/"
 	port := "8080"
 	mux := http.NewServeMux()
 	server := &http.Server{
@@ -14,8 +15,19 @@ func main() {
 		Addr:    ":" + port,
 	}
 
+	//need to update the root dir line to /app/ instead of /
 	mux.Handle(filepathRoot, http.FileServer(http.Dir(".")))
+
 	mux.Handle(filepathRoot+"assets", http.FileServer(http.Dir("./assets")))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte("OK")) // not sure if this is okay to do this way
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 
